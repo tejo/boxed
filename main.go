556b1bc9110006c3c0011926dbee1b1adf2803f0
Matteo Parmi <m.parmi@gmail.com>
2015-01-18 22:53:38 +0100

    try with another go package

--- main.go
+++ main.go
@@ -4,32 +4,29 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"net/url"
 
 	"github.com/gorilla/sessions"
 	"github.com/julienschmidt/httprouter"
-	"github.com/scottferg/Dropbox-Go/dropbox"
+	"github.com/tejo/dropbox"
 )
 
-var s dropbox.Session
 var store = sessions.NewCookieStore([]byte("182hetsgeih8765$aasdhj"))
 
 type User struct {
 	DropboxID string
 	Name      string
-	Token     dropbox.AccessToken
 }
 
-func NewDropboxSession() dropbox.Session {
-	return dropbox.Session{
-		AppKey:     "72ton4woqnari86",
-		AppSecret:  "12a0p8gtsg7fp3i",
-		AccessType: "app_folder",
-	}
+var AppToken = dropbox.AppToken{
+	Key:    "72ton4woqnari86",
+	Secret: "12a0p8gtsg7fp3i",
 }
+
+var RequestToken dropbox.RequestToken
+var AccessToken dropbox.AccessToken
 
 func main() {
-	s = NewDropboxSession()
-
 	router := httprouter.New()
 	router.GET("/", Index)
 	router.GET("/login", Login)
@@ -38,74 +35,31 @@ func main() {
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
 func Login(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-	s.ObtainRequestToken()
-	http.Redirect(w, r, dropbox.GenerateAuthorizeUrl(s.Token.Key, &dropbox.Parameters{
-		OAuthCallback: "http://localhost:8080/oauth/callback",
-	}), 302)
+	RequestToken, _ = dropbox.StartAuth(AppToken)
+	fmt.Printf("dropbox.StartAuth() = %+v\n", RequestToken)
+	u, _ := url.Parse("http://localhost:8080/oauth/callback")
+	authUrl := dropbox.GetAuthorizeURL(RequestToken, u)
+	http.Redirect(w, r, authUrl.String(), 302)
 }
 
 func Callback(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-	session, _ := store.Get(r, "godropblog")
-	token, _ := s.ObtainAccessToken()
-	session.Values["key"] = token.Key
-	session.Values["secret"] = token.Secret
-	session.Save(r, w)
+	AccessToken, _ = dropbox.FinishAuth(AppToken, RequestToken)
+	fmt.Printf("AccessToken = %+v\n", AccessToken)
 	http.Redirect(w, r, "/", 302)
 }
 
 func Index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-	session, _ := store.Get(r, "godropblog")
-
-	if key, secret := session.Values["key"], session.Values["secret"]; key == nil && secret == nil {
-		http.Redirect(w, r, "/login", 302)
-		return
-	} else {
-		s.Token.Key = key.(string)
-		s.Token.Secret = secret.(string)
-	}
-
-	dropbox.CreateFolder(
-		s,
-		dropbox.Uri{
-			Root: "sandbox",
-			Path: "/published",
-		},
-		&dropbox.Parameters{},
-	)
-
-	account, err := dropbox.GetAccount(s, &dropbox.Parameters{})
-
-	if err != nil {
-		//access toke is not valid anymore
-		fmt.Fprintf(w, " %+v\n", err)
-		// reset all session
-		s = NewDropboxSession()
-		session.Values["key"], session.Values["secret"] = "", ""
-		session.Save(r, w)
-		return
-	}
-
-	fmt.Printf("account = %+v\n", account)
-
-	dropbox.CreateFolder(
-		s,
-		dropbox.Uri{
-			Root: "sandbox",
-			Path: "/draft",
-		},
-		&dropbox.Parameters{},
-	)
-
-	p := &dropbox.Parameters{
-		FileLimit: "10000",
-		List:      "true",
-	}
-
-	u := dropbox.Uri{
-		Root: "sandbox",
-		Path: "/",
-	}
-
-	data, err := dropbox.GetMetadata(s, u, p)
-	fmt.Fprintf(w, " %+v\n %+v\n", data, err)
+	db := &dropbox.Client{
+		AppToken:    AppToken,
+		AccessToken: AccessToken,
+		Config: dropbox.Config{
+			Access: dropbox.AppFolder,
+			Locale: "us",
+		}}
+	info, err := db.GetAccountInfo()
+	fmt.Printf("err = %+v\n", err)
+	fmt.Printf("err = %+v\n", info)
+	db.CreateDir("drafts")
+	db.CreateDir("published")
+	db.GetDelta()
 }
