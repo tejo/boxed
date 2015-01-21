688e54829b940d6a45caa44b919586488d6decdb
Matteo Parmi <m.parmi@gmail.com>
2015-01-21 10:36:28 +0100

    puts key and secret in session

--- main.go
+++ main.go
@@ -26,6 +26,8 @@ var AppToken = dropbox.AppToken{
 var RequestToken dropbox.RequestToken
 var AccessToken dropbox.AccessToken
 
+var callbackUrl = "http://localhost:8080/oauth/callback"
+
 func main() {
 	router := httprouter.New()
 	router.GET("/", Index)
@@ -36,19 +38,31 @@ func main() {
 }
 func Login(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	RequestToken, _ = dropbox.StartAuth(AppToken)
-	fmt.Printf("dropbox.StartAuth() = %+v\n", RequestToken)
-	u, _ := url.Parse("http://localhost:8080/oauth/callback")
-	authUrl := dropbox.GetAuthorizeURL(RequestToken, u)
+	url, _ := url.Parse(callbackUrl)
+	authUrl := dropbox.GetAuthorizeURL(RequestToken, url)
 	http.Redirect(w, r, authUrl.String(), 302)
 }
 
 func Callback(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	session, _ := store.Get(r, "godropblog")
 	AccessToken, _ = dropbox.FinishAuth(AppToken, RequestToken)
+	session.Values["key"] = AccessToken.Key
+	session.Values["secret"] = AccessToken.Secret
+	session.Save(r, w)
 	fmt.Printf("AccessToken = %+v\n", AccessToken)
 	http.Redirect(w, r, "/", 302)
 }
 
 func Index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	session, _ := store.Get(r, "godropblog")
+
+	if key, secret := session.Values["key"], session.Values["secret"]; key == nil && secret == nil {
+		// http.Redirect(w, r, "/login", 302)
+		return
+	} else {
+		AccessToken.Key = key.(string)
+		AccessToken.Secret = secret.(string)
+	}
 	db := &dropbox.Client{
 		AppToken:    AppToken,
 		AccessToken: AccessToken,
@@ -57,9 +71,22 @@ func Index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 			Locale: "us",
 		}}
 	info, err := db.GetAccountInfo()
+
+	if err != nil {
+		//access token is not valid anymore
+		fmt.Fprintf(w, " %+v\n", err)
+		// reset all session
+		session.Values["key"], session.Values["secret"] = "", ""
+		session.Save(r, w)
+		// http.Redirect(w, r, "/login", 302)
+		return
+	}
+
 	fmt.Printf("err = %+v\n", err)
 	fmt.Printf("err = %+v\n", info)
 	db.CreateDir("drafts")
 	db.CreateDir("published")
-	db.GetDelta()
+	delta, err := db.GetDelta()
+	fmt.Printf("delta = %+v\n", delta)
+	fmt.Printf("delta err = %+v\n", err)
 }
