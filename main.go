b1cee289473f30debad29c185c7232c2e8c68638
Matteo Parmi <m.parmi@gmail.com>
2015-01-26 10:09:25 +0100

    saves articles

--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"bytes"
 	"encoding/gob"
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"net/url"
@@ -12,6 +14,8 @@ import (
 	"github.com/boltdb/bolt"
 	"github.com/gorilla/sessions"
 	"github.com/julienschmidt/httprouter"
+	"github.com/markbates/going/wait"
+	"github.com/shurcooL/go/github_flavored_markdown"
 	"github.com/tejo/dropbox"
 )
 
@@ -20,10 +24,15 @@ var AppToken = dropbox.AppToken{
 	Secret: "0k1q9zpbt1x3czk",
 }
 
+const (
+	defaultUserId = "5384198"
+)
+
 var callbackUrl = "http://localhost:8080/oauth/callback"
 var db *bolt.DB
 
 func withSession(w http.ResponseWriter, r *http.Request, fn func(*sessions.Session)) {
+	gob.Register(dropbox.RequestToken{})
 	store := sessions.NewCookieStore([]byte("182hetsgeih8765$aasdhj"))
 	store.Options = &sessions.Options{
 		Path:     "/",
@@ -48,7 +57,13 @@ func init() {
 		}
 		return nil
 	})
-	gob.Register(dropbox.RequestToken{})
+	db.Update(func(tx *bolt.Tx) error {
+		_, err := tx.CreateBucketIfNotExists([]byte("UserArticles"))
+		if err != nil {
+			return fmt.Errorf("create bucket: %s", err)
+		}
+		return nil
+	})
 }
 
 func main() {
@@ -57,10 +72,51 @@ func main() {
 	router := httprouter.New()
 	router.GET("/", Index)
 	router.GET("/login", Login)
+	router.GET("/account", Account)
+	router.GET("/r", Refresh)
 	router.GET("/oauth/callback", Callback)
 
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
+
+type Article struct {
+	Key     string
+	Content string
+	dropbox.FileMetadata
+}
+
+func Refresh(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	var at dropbox.AccessToken
+	db.View(func(tx *bolt.Tx) error {
+		b := tx.Bucket([]byte("UserData"))
+		token := b.Get([]byte(defaultUserId + ":token"))
+		json.Unmarshal(token, &at)
+		fmt.Printf("The answer is: %s\n", at)
+		return nil
+	})
+	meta, _ := dbClient(at).GetMetadata("/published", true)
+	fmt.Printf("meta.Contents = %+v\n", meta.Contents)
+	wait.Wait(len(meta.Contents), func(index int) {
+		entry := meta.Contents[index]
+		if entry.IsDir {
+			return
+		}
+		file, _ := dbClient(at).GetFile(entry.Path)
+		content, _ := ioutil.ReadAll(file)
+		article := &Article{
+			Key:          defaultUserId + ":article:" + entry.Path,
+			Content:      string(github_flavored_markdown.Markdown(content)),
+			FileMetadata: entry,
+		}
+		db.Update(func(tx *bolt.Tx) error {
+			b := tx.Bucket([]byte("UserArticles"))
+			a, err := json.Marshal(article)
+			err = b.Put([]byte(article.Key), []byte(a))
+			return err
+		})
+	})
+}
+
 func Login(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	withSession(w, r, func(session *sessions.Session) {
 		RequestToken, _ := dropbox.StartAuth(AppToken)
@@ -72,11 +128,14 @@ func Login(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	})
 }
 
+// saves the user id in session, save used data and access token in
+// db creates the default folders
 func Callback(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	withSession(w, r, func(session *sessions.Session) {
 		RequestToken := session.Values["RequestToken"].(dropbox.RequestToken)
 		AccessToken, _ := dropbox.FinishAuth(AppToken, RequestToken)
-		info, err := dbClient(AccessToken).GetAccountInfo()
+		dbc := dbClient(AccessToken)
+		info, err := dbc.GetAccountInfo()
 		if err != nil {
 			log.Println(err)
 		}
@@ -91,17 +150,31 @@ func Callback(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 		})
 		session.Values["uid"] = info.Uid
 		session.Save(r, w)
-		fmt.Printf("AccessToken = %+v\n", AccessToken)
+		dbc.CreateDir("drafts")
+		dbc.CreateDir("published")
 		http.Redirect(w, r, "/", 302)
 	})
 }
 
 func Index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	db.View(func(tx *bolt.Tx) error {
+		c := tx.Bucket([]byte("UserArticles")).Cursor()
+
+		prefix := []byte(defaultUserId + ":article:")
+		for k, v := c.Seek(prefix); bytes.HasPrefix(k, prefix); k, v = c.Next() {
+			fmt.Fprintf(w, "key=%s, value=%s\n", k, v)
+		}
+
+		return nil
+	})
+}
+
+func Account(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	withSession(w, r, func(session *sessions.Session) {
 		var AccessToken dropbox.AccessToken
 
 		if uid := session.Values["uid"]; uid == nil {
-			log.Println("no uid found")
+			fmt.Fprint(w, "no uid found")
 			return
 		} else {
 			uid := strconv.Itoa(int(session.Values["uid"].(uint64)))
@@ -109,30 +182,20 @@ func Index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 				b := tx.Bucket([]byte("UserData"))
 				token := b.Get([]byte(uid + ":token"))
 				json.Unmarshal(token, &AccessToken)
-				fmt.Printf("The answer is: %s\n", AccessToken)
 				return nil
 			})
 		}
 		db := dbClient(AccessToken)
 		info, err := db.GetAccountInfo()
-		fmt.Printf("err = %+v\n", err)
-
 		if err != nil {
-			//access token is not valid anymore
-			fmt.Fprintf(w, " %+v\n", err)
-			// reset all session
-			session.Values["key"], session.Values["secret"] = "", ""
-			// http.Redirect(w, r, "/login", 302)
+			// access token is not valid anymore
+			// reset session
+			session.Values["uid"] = ""
+			session.Save(r, w)
+			fmt.Fprint(w, "access token not valid")
 			return
 		}
-
-		fmt.Printf("err = %+v\n", err)
-		fmt.Printf("err = %+v\n", info)
-		db.CreateDir("drafts")
-		db.CreateDir("published")
-		delta, err := db.GetDelta()
-		fmt.Printf("delta = %+v\n", delta)
-		fmt.Printf("delta err = %+v\n", err)
+		fmt.Fprintf(w, "info = %+v\n", info)
 	})
 }
 
